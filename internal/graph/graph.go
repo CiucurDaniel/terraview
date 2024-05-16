150ff389381bdd3142a9fddcc5723327bd2223b1
Ciucur Daniel <ciucur.daniel14@gmail.com>
2024-05-16 23:16:25 +0300

    Implement adding images to resource nodes

--- internal/graph/graph.go
+++ internal/graph/graph.go
@@ -11,7 +11,15 @@ import (
 	"strings"
 )
 
-const LABEL_LOCATION = "b"
+const (
+	LABEL_LOCATION = "b"
+
+	// GlobalImagePath is the path where the images are stored.
+	GlobalImagePath = "internal/icons/azurerm"
+)
+
+// KnownProviders is a constant array containing known provider prefixes
+var KnownProviders = []string{"azurerm", "aws", "gcp"}
 
 // ObtainGraph invokes "terraform graph" command in the specified directory
 // and returns the graph data as a string.
@@ -39,8 +47,24 @@ func ObtainGraph(dirPath string) (string, error) {
 	return out.String(), nil
 }
 
+// IsResourceNode checks if the label represents a resource node based on the known provider prefixes.
+// It returns true if the label starts with any of the known prefixes, otherwise false.
+func IsResourceNode(label string) bool {
+	for _, provider := range KnownProviders {
+		if strings.HasPrefix(label, provider+"_") {
+			return true
+		}
+	}
+	return false
+}
+
+// SetGraphGlobalImagePath sets the global image path for the graph.
+func SetGraphGlobalImagePath(graph *gographviz.Graph, path string) {
+	graph.Attrs.Add("imagepath", fmt.Sprintf(`"%s"`, path))
+}
+
 // TODO: Needs fixing, currently it break the formatting because the added label is not correct
-// AddImageLabel appends an image label to every node in the graph where the value
+// AddImageLabel appends an image label to every resource node in the graph where the value
 // is constructed based on the label of the node.
 func AddImageLabel(graph *gographviz.Graph) {
 	for _, node := range graph.Nodes.Nodes {
@@ -50,21 +74,22 @@ func AddImageLabel(graph *gographviz.Graph) {
 		// Remove existing quotes, if any
 		label = strings.Trim(label, `"`)
 
-		// Split the label by underscore to extract the relevant part
-		parts := strings.Split(label, ".")
+		// Check if the node represents a resource
+		if IsResourceNode(label) {
+			// Split the label by underscore to extract the relevant part
+			parts := strings.Split(label, ".")
 
-		// If the label is empty or doesn't contain an underscore, skip the node
-		if len(parts) < 2 {
-			continue
+			// If the label is empty or doesn't contain a dot, skip the node
+			if len(parts) < 2 {
+				continue
+			}
+
+			// Construct the image label path
+			imageLabel := filepath.Join(parts[0] + ".png")
+
+			// Set the image label attribute
+			node.Attrs["image"] = fmt.Sprintf(`"%s"`, imageLabel)
 		}
-
-		// Construct the image label path
-		// TODO: svg is not supported (on my computer by running dot cli)
-		// Maybe a native go rendering library will support it
-		imageLabel := filepath.Join("internal", "icons", "azurerm", parts[0]+".png")
-
-		// Set the image label attribute
-		node.Attrs["image"] = fmt.Sprintf(`"%s"`, imageLabel)
 	}
 }
 
@@ -81,29 +106,6 @@ func PositionLabelTo(graph *gographviz.Graph, position string) {
 		// Set the labelloc attribute of the node to the specified position
 		node.Attrs["labelloc"] = fmt.Sprintf(`"%s"`, position)
 	}
-}
-
-// PrepareGraphForPrinting is a facade function for preparing the graph for printing.
-// It obtains the graph data, adds image labels to nodes, and returns the modified graph.
-func PrepareGraphForPrinting(dirPath string) (*gographviz.Graph, error) {
-
-	graphData, err := ObtainGraph(dirPath)
-	if err != nil {
-		return nil, fmt.Errorf("failed to obtain graph data: %v", err)
-	}
-
-	graph, err := gographviz.Read([]byte(graphData))
-	if err != nil {
-		return nil, fmt.Errorf("failed to parse graph data: %v", err)
-	}
-
-	// Add image labels to nodes
-	AddImageLabel(graph)
-
-	// Set position for labels
-	PositionLabelTo(graph, LABEL_LOCATION)
-
-	return graph, nil
 }
 
 // SaveGraphAsJPEG saves the given graph as a JPEG image.
@@ -133,3 +135,24 @@ func SaveGraphAsJPEG(graph *gographviz.Graph, filePath string) error {
 
 // TODO: Create func which puts consecutive identical resources on same rank
 // similar to what was done here: https://stackoverflow.com/questions/58832678/how-to-separate-picture-and-label-of-a-node-with-graphviz
+
+// PrepareGraphForPrinting is a facade function for preparing the graph for printing.
+// It obtains the graph data, adds image labels to nodes, and returns the modified graph.
+func PrepareGraphForPrinting(dirPath string) (*gographviz.Graph, error) {
+
+	graphData, err := ObtainGraph(dirPath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to obtain graph data: %v", err)
+	}
+
+	graph, err := gographviz.Read([]byte(graphData))
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse graph data: %v", err)
+	}
+
+	SetGraphGlobalImagePath(graph, GlobalImagePath)
+	AddImageLabel(graph)
+	PositionLabelTo(graph, LABEL_LOCATION)
+
+	return graph, nil
+}
